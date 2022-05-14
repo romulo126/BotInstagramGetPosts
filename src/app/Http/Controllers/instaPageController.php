8621a071ce8e100c9eeade9decb63c9f18ba0422
romulohenrique-egsys <romulohenrique-egsys>
2022-05-14 12:31:18 -0300

    Return in json

--- src/app/Http/Controllers/instaPageController.php
+++ src/app/Http/Controllers/instaPageController.php
@@ -5,20 +5,40 @@ namespace App\Http\Controllers;
 use App\Http\Requests\instaPageRequest;
 use App\Server\Instagram\Page\instaPageService;
 
+
 class instaPageController extends Controller
 {
     public function __construct()
     {
-        $this->instaPageService = new instaPageService();
+        $this->InstaPageService = new instaPageService();
     }
 
     public function all(instaPageRequest $request)
     {
-        dd($this->instaPageService->requestPage($request->url, $request->sessionid));
+        try
+        {
+            $resposta['success'] = true;
+            $resposta['data'] =$this->InstaPageService->requestPage($request->url,  getenv('SESSIONIDINSTAGRAM',false) );
+            $resposta['total'] = count($resposta['data']);
+            return response()->json($resposta, 200);
+        }
+        catch (\Exception $e)
+        {
+            return response()->json(['success'=>false,'error' => 'Erro ao buscar página'], 500);
+        }
+        
     }
 
     public function limit($limit, instaPageRequest $request)
     {
-        dd($this->instaPageService->requestPage($request->url, $request->sessionid, $limit));
+        try
+        {
+            $resposta['success'] = true;
+            $resposta['data'] = $this->InstaPageService->requestPage($request->url, getenv('SESSIONIDINSTAGRAM',false) , $limit);
+            $resposta['total'] = count($resposta['data']);
+            return response()->json($resposta, 200);
+        }catch(\Exception $e){
+            return response()->json(['success'=>false,'error' => 'Erro ao buscar página'], 500);
+        }
     }
 }
